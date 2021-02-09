1262e9353f02e462ad5e4ccb6a66bc4f6c56cb92
olga <itsglimerry@gmail.com>
2021-02-09 14:59:33 +0000

    Refactoring magic numbers

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,20 +1,34 @@
 require 'oystercard'
 
 describe OysterCard do
+ test_value = 50
+
+
+ before(:each) do
+     @oystercard = OysterCard.new
+   end
+
+ describe '#top_up' do
+
   it "has a default balance of 0 when initialized" do
-    expect(subject.balance).to eq(0)
+    expect(@oystercard.balance).to eq(0)
   end
  it "tops up with the top up value as the argument" do
-   subject.top_up(50)
-   expect(subject.balance).to eq 50
+   @oystercard.top_up(test_value)
+   expect(@oystercard.balance).to eq test_value
  end
  it "raises an error when top up value exceeds maximum value" do
-   expect { subject.top_up(91) }.to raise_error "Your balance cannot be over £90"
+   expect { @oystercard.top_up(91) }.to raise_error "Your balance cannot be over £90"
  end
- it "deducts money from balance when u spend money" do
+end
+
+describe '#deduct' do
+ it "deducts money from balance when you spend money" do
    oyster_card = OysterCard.new
-   oyster_card.top_up(45)
-   oyster_card.deduct(20)
-   expect(oyster_card.balance).to eq 25
+   oyster_card.top_up(test_value)
+   oyster_card.deduct(test_value)
+   expect(oyster_card.balance).to eq 0
  end
+end
+
 end
