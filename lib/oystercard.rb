e7c0d854ee811d1fe263113aeb9ce38772748dcb
olga <itsglimerry@gmail.com>
2021-02-09 16:19:49 +0000

    Raises an error when balance < single_journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class OysterCard
 DEFAULTBALANCE = 0
 MAXBALANCE = 90
-
+SINGLE_JOURNEY = 1
 attr_reader :balance
 
-  def initialize(balance = DEFAULTBALANCE)
+  def initialize(balance = DEFAULTBALANCE,single_journey = SINGLE_JOURNEY)
     @balance = balance
     @in_journey = false
+    @single_journey = single_journey
   end
 
   def top_up(top_up_value)
@@ -19,12 +20,13 @@ attr_reader :balance
   end
 
   def touch_in
+    raise "Not enough balance" if @balance < @single_journey
     @in_journey = true
   end
 
   def touch_out
     @in_journey = false
-  end 
+  end
 
   def in_journey?
     @in_journey
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -3,12 +3,13 @@ require 'oystercard'
 describe OysterCard do
  test_value = 50
 
-
  before(:each) do
      @oystercard = OysterCard.new
    end
 
  describe '#top_up' do
+
+
 
   it "has a default balance of 0 when initialized" do
     expect(@oystercard.balance).to eq(0)
@@ -18,11 +19,15 @@ describe OysterCard do
    expect(@oystercard.balance).to eq test_value
  end
  it "raises an error when top up value exceeds maximum value" do
-   expect { @oystercard.top_up(91) }.to raise_error "Your balance cannot be over £90"
+   @limit = OysterCard::MAXBALANCE
+   @single_journey = OysterCard::SINGLE_JOURNEY
+   @oystercard.top_up(@single_journey)
+   expect { @oystercard.top_up(@limit) }.to raise_error "Your balance cannot be over £90"
  end
 end
 
 describe '#deduct' do
+
  it "deducts money from balance when you spend money" do
    @oystercard.top_up(test_value)
    @oystercard.deduct(test_value)
@@ -32,16 +37,22 @@ end
 
 describe '#touch_in' do
   it "says you're in a journey when you touch in but don't touch out" do
+  @oystercard.top_up(1)
   @oystercard.touch_in
   expect(@oystercard.in_journey?).to eq true
+  end
+
+  it"raises an error if there isn't enough balance for single journey" do
+    expect{ @oystercard.touch_in }.to raise_error 'Not enough balance'
   end
 end
 
 describe '#touch_out' do
   it "says you are not in a journey after touching in and touching out" do
+    @oystercard.top_up(1)
     @oystercard.touch_in
     @oystercard.touch_out
-    expect(@oystercard.in_journey?).to eq false  
+    expect(@oystercard.in_journey?).to eq false
   end
 end
 
