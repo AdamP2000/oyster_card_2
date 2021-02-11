0ffe4c6340d65a5e29fd5cb3c5d0dc3c1d277cf0
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-11 17:30:20 +0100

    create unit tests for journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,9 +2,12 @@ require_relative 'station'
 
 
 class Journey
+attr_reader :entry_station, :exit_station
 
-
-
+def initialize(entry_station)
+@entry_station = entry_station
+@exit_station = nil
+end
 
 def complete?
   false
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,9 +1,22 @@
 require 'journey'
 
 describe Journey do
+  let(:journey) {Journey.new("station")}
 
 it 'has an uncompleted journey by default' do
-  expect(Journey.new.complete?).to eq false
+  expect(journey.complete?).to eq false
+end
+
+it 'has an entry station' do
+  expect(journey.entry_station).to eq "station"
+end
+
+it 'has no exit station by default' do
+  expect(journey.exit_station).to eq nil
+end
+
+it 'finishes the journey' do
+  expect(journey.end).to eq 
 end
 
 end
